85a205e53f418d26f5050756c6b874efb705d5ae
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-16 08:28:35 -0300

    test: add order test

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def user_factory do
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       description: "Pizza de frango com catupiry",
       quantity: 1,
       unity_price: Decimal.new("32.90")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua dos mafagafos, 22",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("73.90"),
+      user_cpf: "12345678911"
     }
   end
 end
