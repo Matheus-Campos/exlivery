238cb30e13b4a9b096f1a4a858b27bf2e3540707
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-17 19:17:37 -0300

    test: add user agent test

---
+++ test/users/agent_test.exs
@@ -0,0 +1,54 @@
+defmodule Exlivery.Users.AgentTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.User
+
+  import Exlivery.Factory
+
+  describe "save/1" do
+    test "saves the user" do
+      user = build(:user)
+
+      UserAgent.start_link(%{})
+
+      assert UserAgent.save(user) == :ok
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      UserAgent.start_link(%{})
+      cpf = "78945612300"
+      {:ok, cpf: cpf}
+    end
+
+    test "when the user is found, returns the user", %{cpf: cpf} do
+      :user
+      |> build(cpf: cpf)
+      |> UserAgent.save()
+
+      response = UserAgent.get(cpf)
+
+      expected_response =
+        {:ok,
+         %User{
+           address: "Rua dos mafagafos, 22",
+           age: 22,
+           cpf: "78945612300",
+           email: "silva.campos.matheus@gmail.com",
+           name: "Matheus Campos"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when the user is not found, returns an error" do
+      response = UserAgent.get("00000000000")
+
+      expected_response = {:error, "User not found"}
+
+      assert response == expected_response
+    end
+  end
+end
