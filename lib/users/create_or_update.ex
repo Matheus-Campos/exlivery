932fc4a7f875735eabb0f722069b84120ce4031a
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-17 19:25:01 -0300

    test: add tests for create or update user module

--- lib/users/create_or_update.ex
+++ lib/users/create_or_update.ex
@@ -8,6 +8,6 @@ defmodule Exlivery.Users.CreateOrUpdate do
     |> save_user()
   end
 
-  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+  defp save_user({:ok, %User{} = user}), do: {UserAgent.save(user), user}
   defp save_user({:error, _reason} = error), do: error
 end
---
+++ test/users/create_or_update_test.exs
@@ -0,0 +1,56 @@
+defmodule Exlivery.Users.CreateOrUpdateTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.{CreateOrUpdate, User}
+
+  import Exlivery.Factory
+
+  describe "call/1" do
+    setup do
+      UserAgent.start_link(%{})
+
+      :ok
+    end
+
+    test "when all params are valid, saves the user" do
+      params = %{
+        name: "Matheus Campos",
+        address: "Rua dos mafagafos, 22",
+        email: "silva.campos.matheus@gmail.com",
+        cpf: "12345678900",
+        age: 22
+      }
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response =
+        {:ok,
+         %User{
+           address: "Rua dos mafagafos, 22",
+           age: 22,
+           cpf: "12345678900",
+           email: "silva.campos.matheus@gmail.com",
+           name: "Matheus Campos"
+         }}
+
+      assert response == expected_response
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = %{
+        name: "Matheus Campos",
+        address: "Rua dos mafagafos, 22",
+        email: "silva.campos.matheus@gmail.com",
+        cpf: "12345678900",
+        age: 17
+      }
+
+      response = CreateOrUpdate.call(params)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
